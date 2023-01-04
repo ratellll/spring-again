b103c4d3b3f4b55c14f3926086d1db47570522ff
ratellll <latellbin@naver.com>
2023-01-04 23:24:21 +0900

    o

--- src/main/java/hi/springagain/controller/HiController.java
+++ src/main/java/hi/springagain/controller/HiController.java
@@ -10,6 +10,8 @@ public class HiController {
     @GetMapping("hi")
     public String hi(Model model) {
         model.addAttribute("data", "hi!!!");
+        model.addAttribute("name", "현빈이에요");
+        model.addAttribute("age", "말안해줄거에요");
         return "hi";
     }
 }
