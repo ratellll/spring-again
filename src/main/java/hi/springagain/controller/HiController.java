039947d0a2659c1dc047dfed590b0bb77ca84f70
ratellll <latellbin@naver.com>
2023-01-05 23:24:07 +0900

    RequeastParam 다중 사용시 &로 하나씩입력하기

--- src/main/java/hi/springagain/controller/HiController.java
+++ src/main/java/hi/springagain/controller/HiController.java
@@ -3,6 +3,7 @@ package hi.springagain.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HiController {
@@ -13,5 +14,13 @@ public class HiController {
         model.addAttribute("name", "현빈이에요");
         model.addAttribute("age", "말안해줄거에요");
         return "hi";
+    }
+
+    @GetMapping("hi-mvc")
+    public String hiMvc(@RequestParam("name") String name, @RequestParam("age") int age, @RequestParam("money") String money, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        model.addAttribute("money", money);
+        return "hi-template";
     }
 }
